70f16e6fefbfa4ed17e42a90adad16e90bdf30f7
ha-wq <76393670+ha-wq@users.noreply.github.com>
2024-09-15 19:55:00 +0500

    contact info always include tg username

--- main.py
+++ main.py
@@ -789,8 +789,8 @@ async def process_edit_contact(message: types.Message, state: FSMContext):
     if await ignore_old_messages(message):
         return
 
-    contact = message.text
-    await state.update_data(contact=contact)
+    contanct_info = f"(telegram username: @{message.from_user.username}) {message.text}"
+    await state.update_data(contact=contact_info)
 
     user_id = message.from_user.id
     supabase.table("telegram").update({"contact": contact}).eq("user_id", user_id).execute()
@@ -907,7 +907,7 @@ async def process_contact(message: types.Message, state: FSMContext):
 
     logging.info("Processing contact")
     contanct_info = f"(telegram username: @{message.from_user.username}) {message.text}"
-    await state.update_data(contact=message.text)
+    await state.update_data(contact=contact_info)
 
     user_data = await fetch_user_data(message.from_user.id) or {}
     response_message = await process_profile_creation(state, user_data, message.from_user.id)
@@ -943,65 +943,6 @@ async def send_all_command(message: types.Message):
     else:
         await message.answer("You are not authorized to use this command.")
 
-
-
-# send all func for admins
-# @main_router.message(Command("send_all"))
-# async def send_all_command(message: types.Message, state: FSMContext):
-#     # Check if the user is the admin
-#     if message.from_user.id == int(admin_id):
-#         await message.answer("Please send the message (text, image, video, etc.) that you want to broadcast to all users.")
-#         # Set a custom state for this admin action, like `Form.broadcast`
-#         await state.set_state("broadcast_message")
-#     else:
-#         await message.answer("You are not authorized to use this command.")
-
-# A dictionary to store user message IDs for the last broadcast
-# last_message_ids = {}  # e.g., {user_id: message_id}
-
-# @main_router.message(Form.broadcast_message)
-# async def send_broadcast(message: types.Message, state: FSMContext):
-#     # global last_message_ids
-#     # last_message_ids = {}  # Reset the dictionary for tracking
-#     print("Yakhyo's message arrived")
-#     # Fetch all users from the database
-#     users = supabase.table("telegram").select("user_id").execute().data
-#     print(users)
-#     # Send the message to all users and store the message IDs for later deletion
-#     for user in users:
-#         try:
-#             user_id = user["user_id"]
-#             print(user_id)
-#             if message.text:
-#                 await bot.send_message(chat_id=user_id, text=message.text)
-#             elif message.photo:
-#                 await bot.send_photo(chat_id=user_id, photo=message.photo[-1].file_id, caption=message.caption)
-#             elif message.video:
-#                 await bot.send_video(chat_id=user_id, video=message.video.file_id, caption=message.caption)
-#             # Store the message ID for deletion purposes
-#             # last_message_ids[user["user_id"]] = msg.message_id
-#         except Exception as e:
-#             print(f"Failed to send message to {user['user_id']}: {e}")
-    
-#     await message.answer("Broadcast sent to all users.")
-#     await state.clear()  # Clear the state after the broadcast
-
-
-# @main_router.message(Command("delete_last"))
-# async def delete_last_message(message: types.Message):
-#     if message.from_user.id == int(admin_id):  # Ensure only admin can use this command
-#         if last_message_ids:
-#             for user_id, message_id in last_message_ids.items():
-#                 try:
-#                     await bot.delete_message(user_id, message_id)
-#                 except Exception as e:
-#                     print(f"Failed to delete message for {user_id}: {e}")
-#             last_message_ids.clear()  # Clear the list after deleting all messages
-#             await message.answer("Last broadcasted message deleted for all users.")
-#         else:
-#             await message.answer("No broadcasted messages found to delete.")
-#     else:
-#         await message.answer("You are not authorized to use this command.")
 
 
 
