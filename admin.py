ad54ae1e9932177912e6b4ca35caaa765df2d32e
Gulnoza Otabekova <165509083+otabekovagulnoza@users.noreply.github.com>
2025-03-25 09:42:05 +0500

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,1 +1,11 @@
-
+import logging
+from supabase import create_client
+from aiogram import Bot, Dispatcher, types, F, Router
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.filters import Command
+from datetime import datetime
+from zoneinfo import ZoneInfo
+from admin import *
