e21c785e6606b9aa8755622847239c8ecc14e4f2
Madyarov27 <abbosmadyarov555@gmail.com>
2025-04-14 21:27:07 +0500

    Add files via upload

---
+++ interests_keyboard.py
@@ -0,0 +1,14 @@
+# Organized as a dictionary of categories
+categories = {
+    "Test Preparation": ["SAT", "ACT", "IELTS", "TOEFL", "Duolingo", "DTM", "Milliy Sertifikat", "GRE", "GMAT"],
+    "Language Exams": ["Turkish", "Arabic", "English", "German", "French"],
+    "AP Classes": ["AP Calculus BC", "AP Calculus AB", "AP Biology", "AP Chemistry", 
+                  "AP Microeconomics", "AP Macroeconomics", "AP Statistics", "AP Psychology", "AP History"],
+    "Olympiads": ["Math", "Informatics", "Chemistry", "Physics", "Biology", "Economics", "Computer Science"],
+    "Exchange Programs": ["FLEX", "UGRAD", "UWC", "PASCH", "ERASMUS"],
+    "Summer Programs": ["YYGS", "YIRLI", "RSI", "PROMYS", "LaunchX", "NYUAD Design Lab", "SUMaC"],
+    "Scholarships": ["DAAD - Germany", "DSU - Italy", "GKS - Korea", "EYUF", "CGS - China", 
+                    "Turkiye Burslari", "Stipendium Hungaricum", "MEXT - Japan"],
+    "Academic Support": ["College Application", "Extracurricular Building", "Writing Research", "Essay Program"],
+    "General Interests": ["Movies", "Art", "Music", "Sports", "Anime", "TV Shows"]
+}
