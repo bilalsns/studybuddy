297beb359e38d301b57aa78697c8f4b40957ab1e
Otajon-Yuldashev <otajonyuldashev0805@gmail.com>
2025-05-11 16:45:42 +0200

    Update interests_keyboard.py
    Updates on the names, added new values

--- interests_keyboard.py
+++ interests_keyboard.py
@@ -1,14 +1,23 @@
 # Organized as a dictionary of categories
 categories = {
-    "Test Preparation": ["SAT", "ACT", "IELTS", "TOEFL", "Duolingo", "DTM", "Milliy Sertifikat", "GRE", "GMAT"],
+    "Test Preparation": ["SAT", "ACT", "IELTS", "TOEFL", "Duolingo",
+                         "DTM", "Milliy Sertifikat", "GRE", "GMAT"],
     "Language Exams": ["Turkish", "Arabic", "English", "German", "French"],
-    "AP Classes": ["AP Calculus BC", "AP Calculus AB", "AP Biology", "AP Chemistry", 
-                  "AP Microeconomics", "AP Macroeconomics", "AP Statistics", "AP Psychology", "AP History"],
-    "Olympiads": ["Math", "Informatics", "Chemistry", "Physics", "Biology", "Economics", "Computer Science"],
-    "Exchange Programs": ["FLEX", "UGRAD", "UWC", "PASCH", "ERASMUS"],
-    "Summer Programs": ["YYGS", "YIRLI", "RSI", "PROMYS", "LaunchX", "NYUAD Design Lab", "SUMaC"],
-    "Scholarships": ["DAAD - Germany", "DSU - Italy", "GKS - Korea", "EYUF", "CGS - China", 
-                    "Turkiye Burslari", "Stipendium Hungaricum", "MEXT - Japan"],
-    "Academic Support": ["College Application", "Extracurricular Building", "Writing Research", "Essay Program"],
-    "General Interests": ["Movies", "Art", "Music", "Sports", "Anime", "TV Shows"]
+    "International School Curriculums": ["AP Calculus BC", "AP Calculus AB", "AP Biology", "AP Chemistry", 
+                                         "AP Microeconomics", "AP Macroeconomics", "AP Statistics", "AP Psychology", "AP History",
+                                         "A level Mathematics", "A level Computer Science", "A level Economics", "A level Chemistry",
+                                         "A level Biology", "A level Physics", "A level Business", "A level Global Perspectives",
+                                         "IB Math Analysis", "IB Business Management", "IB Biology", "IB Economics", "IB Economics (P)", 
+                                         "IB Physics", "IB Chemistry", "IB Math APPS", "IB Geography", "IB Spanish A Lit", "IB Music",
+                                         "IB Spanish AB.", "IB Visual Arts", "IB English A LAL", "IB English B", "IB French AB.", "IB French B", 
+                                         "IB Theatre", "IB History", "IB Korean A Lit","IB Theory Knowledge", "IB Russian A Lit", "IB Russian B", "IB Russian AB."],
+"Olympiads": ["Math", "Informatics", "Chemistry", "Physics", "Biology", "Economics", "Computer Science"],
+"International Programs and Scholarships": ["FLEX", "UGRAD", "UWC", "PASCH", "ERASMUS", "YYGS", "YIRLI", "RSI", "PROMYS", "LaunchX",
+                                                "NYUAD Design Lab", "SUMaC", "ROSS", "DAAD - Germany", "DSU - Italy", "GKS - Korea",
+                                                "EYUF","Chevening scholarship" "CGS - China", "Turkiye Burslari", "Stipendium Hungaricum",
+                                                "MEXT - Japan", "Lester B. Scholarship", "UBC Scholarship"],
+"Other Academic Interests": ["Undergraduate Admissions to foreign universities", "Graduate Admissions to foreign universities",
+                                 "Application to International Programs", "Research Paper", "Conducting a research Project"],
+
+    "General Interests": ["Movies", "TV Shows", "Cartoons", "Art", "Cooking", "Gaming", "Running", "Football", "Basketball", "Tennis", "UFC", "Boxing"]
 }
